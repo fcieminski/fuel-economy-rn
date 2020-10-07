5fec69fb7a87792f8e663574b17464ee86735231
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-10-07 22:40:04 +0200

    working on better bottom nav

--- navigation/TabNavigation.tsx
+++ navigation/TabNavigation.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  createMaterialTopTabNavigator,
+  MaterialTopTabBarProps,
+} from '@react-navigation/material-top-tabs';
 import { Icon } from 'react-native-elements';
 import MainScreen from '../screens/MainScreen';
 import AddButton from '../components/AddButton';
@@ -7,29 +11,91 @@ import ArchiveScreen from '../screens/ArchiveScreen';
 import FixListScreen from '../screens/FixListScreen';
 import NotesScreen from '../screens/NotesScreen';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+import { View, TouchableOpacity, Text } from 'react-native';
+
+const MyTabBar: React.FC<MaterialTopTabBarProps> = ({
+  state,
+  descriptors,
+  navigation,
+  position,
+}) => {
+  const fixedState = [...state.routes];
+
+  fixedState.splice(2, 0, { name: 'AddButton' });
+
+  const first = () => {
+    return fixedState.map((route, index) => {
+      const info = descriptors[route.key];
+      const label = info?.options.tabBarLabel;
+
+      const isFocused = state.index === index;
+
+      //working on focusing elements
+
+      const onPress = () => {
+        const event = navigation.emit({
+          type: 'tabPress',
+          target: route.key,
+          canPreventDefault: true,
+        });
+
+        if (!isFocused && !event.defaultPrevented) {
+          navigation.navigate(route.name);
+        }
+      };
+
+      const onLongPress = () => {
+        navigation.emit({
+          type: 'tabLongPress',
+          target: route.key,
+        });
+      };
+
+      return (
+        <>
+          {route.name === 'AddButton' ? (
+            <AddButton state={state} />
+          ) : (
+            <TouchableOpacity
+              key={index}
+              accessibilityRole="button"
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1 }}>
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: isFocused ? 'white' : 'black',
+                }}>
+                {info?.options.tabBarIcon()}
+                <Text>{label}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        </>
+      );
+    });
+  };
+
+  return <View style={{ flexDirection: 'row' }}>{first()}</View>;
+};
 
 const TabNavigation: React.FC = () => {
   return (
     <>
       <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: 'white',
-          activeBackgroundColor: '#32a899',
-          inactiveTintColor: '#346358',
-          labelStyle: { fontSize: 14 },
-          style: {
-            backgroundColor: '#e3e3e3',
-            zIndex: 10,
-          },
-        }}
+        tabBarPosition="bottom"
+        tabBar={(props) => <MyTabBar {...props} />}
         initialRouteName="Main">
         <Tab.Screen
           name="Main"
           options={{
             tabBarLabel: 'Spalanie',
-            tabBarIcon({ color }) {
-              return <Icon color={color} type="material-community" name="gas-station" />;
+            tabBarIcon() {
+              return <Icon color="#32a899" type="material-community" name="gas-station" />;
             },
           }}
           component={MainScreen}
@@ -38,25 +104,25 @@ const TabNavigation: React.FC = () => {
           name="Archive"
           options={{
             tabBarLabel: 'Archiwum',
-            tabBarIcon({ color }) {
-              return <Icon color={color} type="material-community" name="archive" />;
+            tabBarIcon() {
+              return <Icon color="#32a899" type="material-community" name="archive" />;
             },
           }}
           component={ArchiveScreen}
         />
-        <Tab.Screen
+        {/* <Tab.Screen
           name="Add"
           component={AddButton}
-          options={{
-            tabBarButton: () => <AddButton />,
-          }}
-        />
+            options={{
+              tabBarButton: () => <AddButton />,
+            }}
+        /> */}
         <Tab.Screen
           name="Notes"
           options={{
             tabBarLabel: 'Notatki',
-            tabBarIcon({ color }) {
-              return <Icon color={color} type="material-community" name="note-text" />;
+            tabBarIcon() {
+              return <Icon color="#32a899" type="material-community" name="note-text" />;
             },
           }}
           component={NotesScreen}
@@ -67,7 +133,7 @@ const TabNavigation: React.FC = () => {
         options={{
           tabBarLabel: 'Przeglądy',
           tabBarIcon({ color }) {
-            return <Icon color={color} type="material-community" name="checkbox-marked-circle" />;
+            return <Icon color="#32a899" type="material-community" name="checkbox-marked-circle" />;
           },
         }}
         component={MainScreen}
@@ -76,8 +142,8 @@ const TabNavigation: React.FC = () => {
           name="FixList"
           options={{
             tabBarLabel: 'Wymiany',
-            tabBarIcon({ color }) {
-              return <Icon color={color} type="material-community" name="wrench" />;
+            tabBarIcon() {
+              return <Icon color="#32a899" type="material-community" name="wrench" />;
             },
           }}
           component={FixListScreen}
